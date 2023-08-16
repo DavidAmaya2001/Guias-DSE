62c7a00c91698bfd26c6a17e5878cd55a8bf4106
TuTioElPollo-Oficial <gamerbloom5@gmail.com>
2023-08-15 19:59:39 -0600

    Subiendo Guia 01

---
+++ Guia 01/MvcPelicula/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            //return "Este es el metodo de acción Bienvenido ...";
+            //return HttpUtility.HtmlEncode("Hola " + nombre + ", NumVeces es: " + numVeces);
+            //return HttpUtility.HtmlEncode("Hola " + nombre + ", ID: " + ID);
+
+            ViewBag.Mensaje = "Hola " + nombre + " " + apellido + ", veces mostrado = ";
+            ViewBag.NumVeces = numVeces;
+            return View();
+        }
+    }
+}
---
+++ Guia 01/Persona/Persona/Controllers/PersonaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Persona.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string nombre, string apellido, string nacimiento, string direccion, string correo, int DUI = 1)
+        {
+            ViewBag.Mensaje = "Hola " + nombre + " " + apellido;
+            ViewBag.DUI = "Numero de DUI: " + DUI;
+            ViewBag.Nacimiento = "Fecha de Nacimiento: " + nacimiento;
+            ViewBag.Direccion = "Direccion: " + direccion;
+            ViewBag.Correo = "Correo Electronico: " + correo;
+            return View();
+        }
+    }
+}
